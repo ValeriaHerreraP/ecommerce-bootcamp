5024276374a0b3bf7adf08b548e31b684f2907ec
ValeriaHerreraP <valeria.herrera015@pascualbravo.edu.co>
2023-05-06 22:43:22 -0500

    feat:insert Products

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -22,13 +22,13 @@
                     <x-nav-link :href="route('users.index')" :active="request()->routeIs('dashboard')">
                         {{ __('Admin User') }}
                     </x-nav-link>
-                    @endcan
                 </div>
 
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-nav-link :href="route('products.index')" :active="request()->routeIs('dashboard')">
                         {{ __('Admin Products') }}
                     </x-nav-link>
+                    @endcan
                 </div>
 
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
