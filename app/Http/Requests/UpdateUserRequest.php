07d6b5aeae3e828d589101a48118c2218bb7da2c
ValeriaHerreraP <valeria.herrera015@pascualbravo.edu.co>
2023-05-06 19:39:55 -0500

    feat:user data validation

---
+++ app/Http/Requests/UpdateUserRequest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateUserRequest extends FormRequest
+{
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name'=> 'required|string|max:60',
+            'lastname'=> 'required|string|max:60',
+            'phone'=> 'required',
+            'email'=> 'required|email',
+            'password'=> 'required',
+        ];
+    }
+}
