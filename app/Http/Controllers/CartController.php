619464de0b7d083b102ca2e09746a3457c0cb684
ValeriaHerreraP <valeria.herrera015@pascualbravo.edu.co>
2023-06-06 11:02:42 -0500

    feat: create shopping cart

---
+++ app/Http/Controllers/CartController.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Products;
+
+class CartController extends Controller
+{
+    public function shop(Request $request)
+    {
+        //muestra los productos 
+        $search = $request->search;
+        $product = products::where('product', 'LIKE', "%{$search}%")->orWhere('price', '<=', "{$search}")->paginate(6);
+
+        return view('cart.shop', ['products' => $product]);
+    }
+
+    public function cart()  {
+        //Se muestran los productos añadidos
+        $cartCollection = \Cart::getContent();
+        //dd($cartCollection);
+        return view('cart.cart', ['cartCollection' => $cartCollection]);;
+    }
+
+    public function remove(Request $request){
+        //Elimina el producto seleccionado
+        \Cart::remove($request->id);
+        return redirect()->route('cart.index')->with('success_msg', 'Item removido del carrito!');
+    }
+
+    public function add(Request$request){
+        //añade el producto al carrito
+        \Cart::add(array(
+            'id' => $request->id,
+            'name' => $request->name,
+            'price' => $request->price,
+            'quantity' => $request->quantity,
+            'attributes' => array(
+                'image' => $request->img,
+        
+            )
+        ));
+        return redirect()->route('cart.index')->with('success_msg', 'Item Agregado a sú Carrito!');
+    }
+
+    public function update(Request $request){
+        //Modifica la cantidad
+        \Cart::update($request->id,
+            array(
+                'quantity' => array(
+                    'relative' => false,
+                    'value' => $request->quantity
+                ),
+        ));
+        return redirect()->route('cart.index')->with('success_msg', 'El carrito fue modificado');
+    }
+
+    public function clear(){
+        //Elimina todos los elementos del carrito
+        \Cart::clear();
+        return redirect()->route('cart.index')->with('success_msg', 'Carrito sin productos.');
+    }
+
+
+
+  /*  public function uu (Request $request)
+    {
+       
+
+        \Cart::add(array(
+            'id' => 456, // inique row ID
+            'name' => 'Sample Item',
+            'price' => 67.99,
+            'quantity' => 4,
+            'attributes' => array()
+        ));
+
+        \Cart::update(456, array(
+            'name' => 'New Item Name', // new item name
+            'price' => 98.67, // new item price, price can also be a string format like so: '98.67'
+          ));
+
+          \Cart::remove(456);
+
+        $cartCollection = \Cart::getContent();
+        dd($cartCollection);
+
+
+        return view('productos', ['products' => $product]);
+
+    }
+
+    public function cart()  {
+
+        $cartCollection = \Cart::getContent();
+        dd($cartCollection);
+        
+        return view('cart');
+    }
+    */
+
+}
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -32,7 +32,7 @@
                 </div>
 
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-nav-link :href="route('productos')" :active="request()->routeIs('dashboard')">
+                    <x-nav-link :href="route('cart.shop')" :active="request()->routeIs('dashboard')">
                         {{ __('Products') }}
                     </x-nav-link>
                 </div>
